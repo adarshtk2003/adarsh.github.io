8a346ce162c84bbe8402adc9bcedaf96c784a877
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-20 14:02:58 -0400

    Created variable and arrays
    I've created one variable to store the text and also added three array.

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -13,3 +13,8 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+const storyText;
+const insertX;
+const insertY;
+const insertZ;
