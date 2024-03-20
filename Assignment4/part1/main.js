1293b7b4b6454ec557635932b7527abead8630a4
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-20 14:08:56 -0400

    updated the value of storyText.
    I've updated the value for variable storyText

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -14,7 +14,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText;
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertX;
 const insertY;
 const insertZ;
