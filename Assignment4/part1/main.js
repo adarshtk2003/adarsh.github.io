cac8993729b4cf05673599e0a529359415710938
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-20 14:59:35 -0400

    Did the UK conversion
    For the stroy, if the user select the UK radio button the values are set to change to UK values.

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -45,8 +45,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature =  `${Math.round((94 - 32) * (5/9))} centigrade`;
 
   }
 
