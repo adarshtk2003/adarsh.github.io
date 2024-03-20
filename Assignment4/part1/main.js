e8e8d9e280f03b920d0bd7213e13b02be39df26b
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-20 15:05:22 -0400

    replaced the UK value.
    I've replaced the UK value conversions to the newStory.

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -47,9 +47,12 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300 / 14)} stone`;
     const temperature =  `${Math.round((94 - 32) * (5/9))} centigrade`;
+    newStory = newStory
+                    .replace("300 pounds", weight)
+                    .replace("94 fahrenheit", temperature)
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
