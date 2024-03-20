f742f8e8758a87108a7e938d51ac7757865deef0
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-20 13:59:05 -0400

    Added Variables and functions
    I've added the variables and functions to the main.

--- Assignment4/part1/main.js
+++ Assignment4/part1/main.js
@@ -3,3 +3,13 @@
 // Date: 20 March, 2024
 // Description: Creating a stroy generator using html and javascript. The different aspects of java
 //              script are used in this.
+
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
