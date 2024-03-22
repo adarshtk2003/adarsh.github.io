4061c1477f6e6a3983307876ce735b563ce7db8d
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-21 22:54:36 -0400

    Declared an array of images.
    In JavaScript I've declared an array that consists of 5 images that we use for this part.

--- Assignment4/part2/main.js
+++ Assignment4/part2/main.js
@@ -4,13 +4,13 @@
 // Date: 20 March, 2024
 // Description: Creating an image gallery and getting more in depth idea about javascript. 
 
-// const displayedImage = document.querySelector('.displayed-img');
+const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
