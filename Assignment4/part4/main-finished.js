ca307e894c477be2eb391ee561d22be004794fac
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2024-03-22 22:23:37 -0400

    Added eventlistener
    I've added the keyboard keys for the move of the evil circle.

--- Assignment4/part4/main-finished.js
+++ Assignment4/part4/main-finished.js
@@ -103,6 +103,24 @@ class EvilCircle extends Shape {
    
    }} 
 
+   window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+   });
+
+
 const balls = [];
 
 while (balls.length < 25) {
